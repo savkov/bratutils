272fb4c5aa0efb334a9391c48deac8c697c2a43f
Aleksandar Savkov <cytehuop@gmail.com>
2014-07-31 09:24:27 +0100

    Added tests and renamed module

---
+++ src/bratutils/bratdata.py
@@ -0,0 +1,399 @@
+# This file is part of DrTokenizer.
+#
+# DrTokenizer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DrTokenizer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DrTokenizer.  If not, see <http://www.gnu.org/licenses/>.
+__author__ = 'Aleksandar Savkov'
+
+"""This module holds data structure classes and relevant methods for parsing and
+manipulating brat annotation files.
+"""
+
+import logging
+
+
+class BratComment(object):
+    """Brat comment container
+    """
+
+    def __init__(self, commentstr):
+        """Creates a new object from a comment line from a brat annotation file.
+
+        :param commentstr: comment line string
+        """
+
+        self.id, self.note, self.recordref = self._parse_comment(commentstr)
+
+    @staticmethod
+    def _parse_comment(c):
+        """Parses comment line string into id, note, and reference
+
+        :param c: comment line string
+        :return: id, note, and reference
+        :rtype: str, str, str
+        """
+        items = c.split("\t")
+        rid = int(items[0][1:])
+        note = items[2]
+        ref = int(items[1][16:])
+        return rid, note, ref
+
+    def __str__(self):
+        return "#{0}\tAnnotatorNotes T{1}\t{2}\n".format(str(self.id),
+                                                         str(self.recordref),
+                                                         self.note)
+
+    def __eq__(self, other):
+        """Returns `True` if the other object is of type `BratComment` and the
+        values of the three attributes `id`, `recordref`, and `note` are
+        identical.
+
+        :param other:
+        :return: True if the objects are the same
+        :rtype: bool
+        """
+        if not isinstance(other, BratComment):
+            return False
+        if self.id != other.id:
+            return False
+        if self.note != other.note:
+            return False
+        if self.recordref != other.recordref:
+            return False
+        return True
+
+    def __hash__(self):
+        """Returns hash based on the `id`, `recordref`, and `note` attributes.
+
+
+        :return: hash
+        """
+        return hash((self.id, self.recordref, self.note))
+
+
+class BratAnnotation(object):
+    """Brat annotation usually recorded on a single line in a .ann file.
+    """
+
+    def __init__(self, s):
+        self.comment = None
+        self.id, self.tag, self.boundaries, self.content = \
+            self._parse_annotation(s)
+
+    @staticmethod
+    def _parse_annotation(a):
+        items = a.split("\t")
+        rid = int(items[0][1:])
+        subitems = items[1].split(" ", 1)
+        tag = subitems[0]
+        boundaries = []
+        for boundarystr in subitems[1].split(";"):
+            boundaries.append(boundarystr.split(" "))
+        content = items[2]
+        return rid, tag, boundaries, content
+
+    def update_id(self, new_id):
+        """Update a record ID and any attached to it comments
+
+        :param new_id: new id
+        """
+        self.id = new_id
+        if self.comment:
+            self.comment.recordref = new_id
+
+    def set_comment(self, comment):
+        """Set comment value
+
+        :param comment: comment
+        """
+        self.comment = comment
+
+    def add_comment(self, comment):
+        """Add new comment to this annotation
+
+        :param comment:
+        """
+        if self.comment and comment:
+            self.comment.note = "Comment 1: {0} Comment 2: {1}".format(
+                self.comment.note.replace("\n", ""), comment.note)
+        elif comment:
+            self.comment = comment
+            self.comment.recordref = self.id
+
+    @property
+    def boundary_str(self):
+        """Returns string representation of the boundaries of this annoation.
+
+
+        :return:
+        """
+        return ";".join([" ".join(x) for x in self.boundaries])
+
+    def get_left_border(self):
+        """Returns left border index of this annotation.
+
+
+        :return: left border index
+        :rtype: int
+        """
+        return int(self.boundaries[0][0])
+
+    def get_right_border(self, inclusive=False):
+        """Returns right border index of this annotation.
+
+        :param inclusive: inclusive border position
+        :return: right border index
+        :rtype: int
+        """
+        return (int(self.boundaries[-1][1])-1
+                if inclusive
+                else int(self.boundaries[-1][1]))
+
+    def __str__(self):
+        comment_str = str(self.comment) if self.comment else ""
+        return "T{0}\t{1} {2}\t{3}\n{4}".format(str(self.id), self.tag,
+                                                ";".join([" ".join(b)
+                                                          for b
+                                                          in self.boundaries]),
+                                                self.content, comment_str)
+
+    def __repr__(self):
+        comment_str = "; {0}".format(str(self.comment)) if self.comment else ""
+        return "T{0}\t{1} {2}\t{3}{4}".format(str(self.id),
+                                              self.tag,
+                                              ";".join([" ".join(b)
+                                                        for b
+                                                        in self.boundaries]),
+                                              self.content, comment_str)
+
+    def __eq__(self, other):
+        """Returns True if the following attributes of both objects are the
+        same: `id`, `tag`, `content`, `comment`, `boundaries`
+
+        :param other: other annotation object
+        :return: True if objects are the same
+        :rtype: bool
+        """
+        if not isinstance(other, BratAnnotation):
+            return False
+        if self.id != other.id:
+            return False
+        if self.tag != other.tag:
+            return False
+        if self.content != other.content:
+            return False
+        if not self.comment == other.comment:
+            return False
+        if self.boundaries != other.boundaries:
+            return False
+        return True
+
+    def __hash__(self):
+        """Returns a hash based on the following attributes: `id`, `tag`,
+        `content`, `comment`, `boundaries`
+
+
+        :return: hash code
+        """
+        return hash((self.id, self.boundaries, self.content, self.comment))
+
+
+class BratDocument(dict):
+    """Annotation file document made up of annotations represented with
+    Annotation objects. This object extends dict. Annotations are stored using
+    their ID as a key value.
+    """
+
+    def __init__(self, fp=None, no_newline=True):
+        """Construct a new object and optionally add content to it from an
+        annotation document file (.ann).
+
+        :param fp: annotation document file path
+        :param no_newline: trim newline char
+        """
+        super(BratDocument, self).__init__()
+        if fp:
+            with open(fp) as doc:
+                for line in doc:
+                    if no_newline:
+                        line = line[0:-1]
+                    if line.startswith("#"):
+                        comment = BratComment(line)
+                        self[comment.recordref].set_comment(comment)
+                    else:
+                        ann = BratAnnotation(line)
+                        self[ann.id] = ann
+
+    def filter_tags(self, filters, positive_polarity=True):
+        """Filter annotations in this document
+
+        :param filters: filter list
+        :param positive_polarity: keep filtered values
+        """
+        for key in self.keys():
+            if (self[key].tag in filters) != positive_polarity:
+                del self[key]
+
+    def unescape_tags(self, escaped_tags_dict):
+        """Replace escaped annotation tags with original values. Brat can't
+        handle certain characters, such as dollar sign in the tag annotation.
+
+        Dictionary example:
+        {'NN_DOLLAR': 'NN$'}
+
+        :param escaped_tags_dict: dictionary of escaped tags
+        """
+        for ann in self.values():
+            if ann.tag in escaped_tags_dict.keys():
+                ann.tag = escaped_tags_dict[ann.tag]
+
+    def remove_duplicates(self):
+        """Removes duplicate annotations in this document.
+
+
+        """
+        duplicates = []
+        for key in self.keys():
+            if key in duplicates:
+                continue
+            new_duplicates = self.get_duplicates(key)
+            for duplicate in new_duplicates:
+                self[key].add_comment(self[duplicate].comment)
+                del self[duplicate]
+            duplicates.extend(new_duplicates)
+
+    def get_duplicates(self, target_key):
+        """Returns a list of keys of duplicate annotations of a target
+        annotation.
+
+        :param target_key: target key
+        :return: keys of duplicate annotations
+        :rtype: list
+        """
+        duplicates = []
+        for key in self.keys():
+            if key == target_key:
+                continue
+            if (self[key].boundaries == self[target_key].boundaries and
+                    self[key].tag == self[target_key].tag):
+                duplicates.append(key)
+        return duplicates
+
+    def enumerate_comments(self):
+        """Enumerate all comments attached to annotations in this document.
+
+
+        """
+        comment_id = 1
+        for record in self.values():
+            if record.comment:
+                record.comment.id = comment_id
+                comment_id += 1
+
+    @property
+    def comments_count(self):
+        """Number of comments in this document.
+
+
+        :return: number of comments
+        :rtype: int
+        """
+        count = 0
+        for record in self.values():
+            if record.comment:
+                count += 1
+        return count
+
+    @property
+    def sorted_values(self):
+        """Returns a list of this documents annotations sorted by their border
+        indices.
+
+
+        :return: list of annotations
+        :rtype: list
+        """
+        return sorted(self.values(), cmp=_comp_brat_ann)
+
+    def validate_indices(self):
+        """Validates the annotation indices in this document.
+
+
+        :return: True if indices are correct
+        :rtype: bool
+        """
+        triples = []
+        for record in self.values():
+            triples.append((record.get_left_border(), True, record))
+            triples.append((record.get_right_border(), False, record))
+        triples.sort()
+        for i, tr in enumerate(triples):
+            look_ahead = 1
+            balance = 0
+            stack = []
+            while tr[1] and tr[2] != triples[i + look_ahead][2]:
+                if triples[i + look_ahead][1]:
+                    balance += 1
+                else:
+                    balance -= 1
+                stack.append(triples[i + look_ahead])
+                look_ahead += 1
+            if balance != 0:
+                print "Stack %s\n%s" % (tr, stack)
+                logging.info("Stack %s\n%s" % (tr, stack))
+                return False
+        return True
+
+    def export_to_file(self, fp):
+        """Exports this document to a file.
+
+        :param fp: file path
+        """
+        with open(fp, "w") as doc:
+            for record in self.values():
+                doc.write(str(record))
+
+    def __eq__(self, other):
+        """Returns True if the key sets and their respective values are the
+        same.
+
+        :param other:
+        :return:
+        """
+        if not isinstance(other, BratDocument):
+            return False
+        if self.keys() != other.keys():
+            return False
+        for key in self.keys():
+            if not self[key] == other[key]:
+                return False
+        return True
+
+
+def _comp_brat_ann(ann_a, ann_b):
+    """Compare brat annotation records by their border indices.
+
+    :param ann_a: annotation A
+    :param ann_b: annotation B
+    :return: 1 a < b; 0 a == b; -1 a > b
+    :rtype: int
+    """
+    hash_a = (ann_a.get_left_border(), ann_a.get_right_border())
+    hash_b = (ann_b.get_left_border(), ann_b.get_right_border())
+
+    if hash_a < hash_b:
+        return 1
+    elif hash_a == hash_b:
+        return 0
+    else:
+        return -1
+
---
+++ src/tests/test_datastructures.py
@@ -0,0 +1,178 @@
+# This file is part of DrTokenizer.
+#
+# DrTokenizer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DrTokenizer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DrTokenizer.  If not, see <http://www.gnu.org/licenses/>.
+__author__ = 'Aleksandar Savkov'
+
+from unittest import TestCase
+from bratutils.bratdata import *
+
+
+class TestDataStructures(TestCase):
+
+    def setUp(self):
+        self.commentStr = "#1	AnnotatorNotes T36	Abbreviation for the " \
+                          "word increase"
+        self.comment2Str = "#2	AnnotatorNotes T45	Another abbreviation for " \
+                           "the word increase"
+        self.recordStr = "T36	MV 681 685	incr"
+        self.record2Str = "T39	NP 798 805;810 819	bla bla"
+        self.record3Str = "T45	NP 681 685	incr"
+
+    def test_comment(self):
+        c = BratComment(self.commentStr)
+        self.assertEquals(c.id, 1)
+        self.assertEquals(c.recordref, 36)
+        self.assertEquals(c.note, "Abbreviation for the word increase")
+
+    def test_record(self):
+        r = BratAnnotation(self.recordStr)
+        self.assertEquals(r.id, 36)
+        self.assertEquals(r.tag, "MV")
+        self.assertEquals(r.boundaries, [["681", "685"]])
+        self.assertEquals(r.content, "incr")
+
+    def test_record_addcomment(self):
+        r = BratAnnotation(self.recordStr)
+        c = BratComment(self.commentStr)
+        c2 = BratComment(self.comment2Str)
+        r.set_comment(c)
+        r.add_comment(c2)
+        self.assertEquals(str(r.comment),
+                          "#1\tAnnotatorNotes T36\tComment "
+                          "1: Abbreviation for the word increase Comment "
+                          "2: Another abbreviation for the word increase\n")
+
+    def test_record_getBoundaryText(self):
+        r = BratAnnotation(self.recordStr)
+        r2 = BratAnnotation(self.record2Str)
+        self.assertEquals(r.boundary_str, "681 685")
+        self.assertEquals(r2.boundary_str, "798 805;810 819")
+
+    def test_record_eq(self):
+        r1 = BratAnnotation(self.recordStr)
+        r2 = BratAnnotation(self.recordStr)
+        self.assertEqual(r1, r2)
+        self.assertListEqual([r1], [r2])
+
+    # def test_tokenvector_getTokenIdx(self):
+    #     tv = BratTokenVector(doc_path="./res/sampleTokenVector.tok")
+    #     token = BratToken("symptomAs\t21\t29")
+    #     idx = tv.get_token_idx(token)
+    #     self.assertEquals(idx, 2)
+    #
+    # def test_tokenvector_insertListAt(self):
+    #     tv = BratTokenVector(doc_path="./res/sampleTokenVector.tok")
+    #     tokenList = [BratToken("foo	10	12"), BratToken("bar	21	23")]
+    #     tok_before = BratToken("distension	10	19")
+    #     tok_after = BratToken("symptomAs	21	29")
+    #     tv.insert_list_at(2, tokenList)
+    #     self.assertEquals(tv[1], tok_before, "Preceding token misatch.")
+    #     self.assertEquals(tv[4], tok_after, "Following token misatch.")
+    #     self.assertEquals(tv[2:4], tokenList, "List tokens mismatch.")
+
+    def test_document_eq(self):
+        doc = BratDocument()
+        doc2 = BratDocument()
+        r1 = BratAnnotation("T1	NP 0 28	Abdominal distension symptom\n")
+        r11 = BratAnnotation("T1	NP 0 28	Abdominal distension symptom\n")
+        c1 = BratComment(
+            "#1	AnnotatorNotes T1	Abbreviation for the word increase #1\n")
+        c11 = BratComment(
+            "#1	AnnotatorNotes T1	Abbreviation for the word increase #1\n")
+        r1.set_comment(c1)
+        r11.set_comment(c11)
+        doc[r1.id] = r1
+        doc2[r11.id] = r11
+        r2 = BratAnnotation("T2	TE 32 41	As before\n")
+        r22 = BratAnnotation("T2	TE 32 41	As before\n")
+        c2 = BratComment("#2	AnnotatorNotes T2	Another abbreviation for "
+                         "the word increase #2\n")
+        c22 = BratComment("#2	AnnotatorNotes T2	Another abbreviation for "
+                          "the word increase #2\n")
+        r2.set_comment(c2)
+        r22.set_comment(c22)
+        doc[r2.id] = r2
+        doc2[r22.id] = r22
+        self.assertEqual(r1, r11)
+        self.assertEqual(r2, r22)
+        self.assertEqual(doc, doc2)
+
+    def test_document(self):
+        doc_from_file = BratDocument("res/tests/sampledoc.ann")
+        doc = BratDocument()
+        r1 = BratAnnotation("T1	NP 0 28	Abdominal distension symptom")
+        c1 = BratComment("#1	AnnotatorNotes T1	Abbreviation for the "
+                         "word increase #1")
+        r1.set_comment(c1)
+        doc[r1.id] = r1
+        r2 = BratAnnotation("T2	TE 32 41	As before")
+        c2 = BratComment("#2	AnnotatorNotes T2	Another abbreviation "
+                         "for the word increase #2")
+        r2.set_comment(c2)
+        doc[r2.id] = r2
+        r3 = BratAnnotation("T3	NP 43 60	fybogel unhelpful")
+        doc[r3.id] = r3
+        r4 = BratAnnotation("T4	NP 0 28	Abdominal distension symptom")
+        c3 = BratComment("#3	AnnotatorNotes T4	Another abbreviation for "
+                         "the word increase #3")
+        r4.set_comment(c3)
+        doc[r4.id] = r4
+        r5 = BratAnnotation("T5	TE 67 77	for a week")
+        doc[r5.id] = r5
+        r6 = BratAnnotation("T6	OE 79 86	On exam")
+        doc[r6.id] = r6
+        r7 = BratAnnotation("T7	NP 43 60	fybogel unhelpful")
+        c4 = BratComment("#3	AnnotatorNotes T7	Another abbreviation for "
+                         "the word increase #4")
+        r7.set_comment(c4)
+        doc[r7.id] = r7
+        self.assertDictEqual(doc, doc_from_file)
+
+    def test_document_get_duplicates(self):
+        doc_from_file = BratDocument("res/tests/sampledoc.ann")
+        duplicates = doc_from_file.get_duplicates(1)
+        self.assertListEqual(duplicates, [4])
+        duplicates = doc_from_file.get_duplicates(2)
+        self.assertListEqual(duplicates, [])
+        duplicates = doc_from_file.get_duplicates(3)
+        self.assertListEqual(duplicates, [7])
+        duplicates = doc_from_file.get_duplicates(4)
+        self.assertListEqual(duplicates, [1])
+
+    def test_document_removeduplicates(self):
+        doc_from_file = BratDocument("res/tests/sampledoc.ann")
+        doc = BratDocument()
+        r1 = BratAnnotation("T1	NP 0 28	Abdominal distension symptom")
+        c1 = BratComment(
+            "#1	AnnotatorNotes T1	"
+            "Comment 1: Abbreviation for the word increase #1 "
+            "Comment 2: Another abbreviation for the word increase #3")
+        r1.set_comment(c1)
+        doc[r1.id] = r1
+        r2 = BratAnnotation("T2	TE 32 41	As before")
+        c2 = BratComment("#2	AnnotatorNotes T2	Another abbreviation for "
+                         "the word increase #2")
+        r2.set_comment(c2)
+        doc[r2.id] = r2
+        r3 = BratAnnotation("T3	NP 43 60	fybogel unhelpful")
+        c4 = BratComment("#3	AnnotatorNotes T3	Another abbreviation for "
+                         "the word increase #4")
+        r3.set_comment(c4)
+        doc[r3.id] = r3
+        r5 = BratAnnotation("T5	TE 67 77	for a week")
+        doc[r5.id] = r5
+        r6 = BratAnnotation("T6	OE 79 86	On exam")
+        doc[r6.id] = r6
+        doc_from_file.remove_duplicates()
+        self.assertDictEqual(doc, doc_from_file)
