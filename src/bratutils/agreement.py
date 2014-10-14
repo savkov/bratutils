fb94987e31f3b8a223502c3feeccbf679523f19d
Aleksandar Savkov <cytehuop@gmail.com>
2014-10-14 13:02:48 +0100

    Fixed header comment and author name.

--- src/bratutils/agreement.py
+++ src/bratutils/agreement.py
@@ -1,17 +1,17 @@
-# This file is part of DrTokenizer.
+# This file is part of BratUtils.
 #
-# DrTokenizer is free software: you can redistribute it and/or modify
+# BratUtils is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# DrTokenizer is distributed in the hope that it will be useful,
+# BratUtils is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with DrTokenizer.  If not, see <http://www.gnu.org/licenses/>.
+# along with BratUtils.  If not, see <http://www.gnu.org/licenses/>.
 __author__ = 'Aleksandar Savkov'
 
 """This module groups data structure classes necessary for the calculation of
