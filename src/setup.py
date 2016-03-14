f10a214fa9b1007459bd7325ea78563039e09973
savkov <aleksandar@savkov.eu>
2016-03-14 22:31:39 +0000

    Added setup installation

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+__author__ = 'Aleksandar Savkov'
+
+setup(
+    name='bratutils',
+    version='0.2',
+    description='brat utilities',
+    author='Aleksandar Savkov',
+    author_email='aleksandar@savkov.eu',
+    packages=find_packages()
+)
