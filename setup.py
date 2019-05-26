62bb1a8bec49943be7984865bd7d594544a094c5
Sasho Savkov <me@sasho.io>
2019-05-26 22:49:05 +0100

    Prepared setup.py for pypi releases

--- setup.py
+++ setup.py
@@ -2,25 +2,39 @@
 
 import os
 
-from setuptools import setup, find_packages
+from distutils.core import setup
+from setuptools import find_packages
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
-with open('VERSION') as f:
-    VERSION = f.read().splitlines()[0]
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='bratutils',
-    version=VERSION,
+    version='0.2.2',
     packages=find_packages('src', exclude=('tests',)),
+    url='https://github.com/savkov/transcriptor',
+    author='Sasho Savkov',
+    author_email='me@sasho.io',
     package_dir={'': 'src'},
     zip_safe=True,
     include_package_data=False,
     description='brat utilities',
-    author='Sasho Savkov',
-    license='GNU',
-    long_description=(
-        'https://github.com/savkov/bratutils'
-    )
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Operating System :: Unix',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering'
+    ]
 )
